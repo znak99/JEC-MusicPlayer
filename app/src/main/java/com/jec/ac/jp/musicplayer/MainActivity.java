c22b80e21655bbb7be79f5ccd13f247a2874dd43
znak99 <xhahekcldhkznak0113@gmail.com>
2023-05-02 14:37:35 +0900

    screen

--- app/src/main/java/com/jec/ac/jp/musicplayer/MainActivity.java
+++ app/src/main/java/com/jec/ac/jp/musicplayer/MainActivity.java
@@ -2,9 +2,14 @@ package com.jec.ac.jp.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
