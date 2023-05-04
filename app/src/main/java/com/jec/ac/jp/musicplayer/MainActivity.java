94875566180f74a53e5479bfd881d5f0c1c98711
znak99 <xhahekcldhkznak0113@gmail.com>
2023-05-04 21:50:33 +0900

    sd card activity

--- app/src/main/java/com/jec/ac/jp/musicplayer/MainActivity.java
+++ app/src/main/java/com/jec/ac/jp/musicplayer/MainActivity.java
@@ -2,18 +2,51 @@ package com.jec.ac.jp.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button playButton;
-    MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
+    private MediaPlayer mediaPlayer;
+    private ImageButton playButton;
+    private TextView currentSelectedMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = findViewById(R.id.xmlPlayButton);
+        playButton.setOnClickListener(new PlayButtonAction());
+
+        currentSelectedMedia = findViewById(R.id.xmlPlayingMusicInfo);
+        currentSelectedMedia.setText("SDカードから音楽を選択してください");
+    }
+
+    private void showAlert(String title, String msg, String btnTxt, DialogInterface.OnClickListener action) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle(title);
+        builder.setMessage(msg);
+        builder.setPositiveButton(btnTxt, action);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    final class PlayButtonAction implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (mediaPlayer == null) {
+                showAlert("", "音楽を選択してください", "確認", null);
+                return;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/jec/ac/jp/musicplayer/SDListActivity.java
@@ -0,0 +1,14 @@
+package com.jec.ac.jp.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SDListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdlist);
+    }
+}
